ec82238c2542b173562586cea829d3e030474278
Alifianrc <62532983+Alifianrc@users.noreply.github.com>
2020-04-14 20:11:04 +0700

    First Upload

---
+++ WPG2/Assets/Script/GlobalVars.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GlobalVars 
+{
+    //public static int CategoryChosen;
+    //public static int score;
+    //public static int ArenaSize = 5;
+    //public static int cardId;
+    //public const int MAXSTAGE = 10;
+    public static char[] itemWordSelected;
+}
